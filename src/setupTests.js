b9cacf098c4db54962d72be4f001e0380919c457
Caitie Garrood <caitlinanneg@gmail.com>
2021-03-24 18:02:45 -0400

    Creates test database and seeds data to run tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,8 @@
 require("./environment");
+
+router.put("/appointments/:id", (request, response) => {
+    if (process.env.TEST_ERROR) {
+      setTimeout(() => response.status(500).json({}), 1000);
+      return;
+    }
+});
